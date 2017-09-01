ba5e0f59d0e018357360b407f50c7aa92da6492b
Kiril Shchasny <kiril3467@yandex.ru>
2017-09-01 16:46:52 +0300

    Dependency resolver added

---
+++ DependencyResolver/ResolverConfig.cs
@@ -0,0 +1,43 @@
+﻿using System.Data.Entity;
+using BLL.Interfaces.Services;
+using BLL.Services;
+using DAL.Concrete;
+using DAL.Interfaces.Repository;
+using EFModel;
+using Ninject;
+
+namespace DependencyResolver
+{
+    public static class ResolverConfig
+    {
+        public static void ConfigurateResolverWeb(this IKernel kernel) => Configure(kernel, true);
+
+        public static void ConfigurateResolverConsole(this IKernel kernel) => Configure(kernel, false);
+
+        private static void Configure(IKernel kernel, bool isWeb)
+        {
+            if (isWeb)
+            {
+                kernel.Bind<IUnitOfWork>().To<UnitOfWork>(); //InRequestScope is unavailable
+                kernel.Bind<DbContext>().To<BloghostContext>();
+            }
+            else
+            {
+                kernel.Bind<IUnitOfWork>().To<UnitOfWork>().InSingletonScope();
+                kernel.Bind<DbContext>().To<BloghostContext>().InSingletonScope();
+            }
+
+            kernel.Bind<IUserService>().To<UserService>();
+            kernel.Bind<IUserRepository>().To<UserRepository>();
+
+            kernel.Bind<IBlogService>().To<BlogService>();
+            kernel.Bind<IBlogRepository>().To<BlogRepository>();
+
+            kernel.Bind<IPostService>().To<PostService>();
+            kernel.Bind<IPostRepository>().To<PostRepository>();
+
+            kernel.Bind<ICommentService>().To<CommentService>();
+            kernel.Bind<ICommentRepository>().To<CommentRepository>();
+        }
+    }
+}
